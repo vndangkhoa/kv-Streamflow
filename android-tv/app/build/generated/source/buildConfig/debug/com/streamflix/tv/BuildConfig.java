9ed95f5b97df46d12c49daadb68ffbede182587c
vndangkhoa <vndangkhoa@users.noreply.github.com>
2026-01-02 23:21:11 +0700

    style: update app icon and banner to YouTube-style Play Button

--- android-tv/app/build/generated/source/buildConfig/debug/com/streamflix/tv/BuildConfig.java
+++ android-tv/app/build/generated/source/buildConfig/debug/com/streamflix/tv/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.streamflix.tv";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0.0";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.3.0";
   // Field from default config.
   public static final String API_BASE_URL = "https://nf.khoavo.myds.me";
 }
